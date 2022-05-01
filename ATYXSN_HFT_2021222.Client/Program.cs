24308a5cd16c67bf41f8a3514270bbd2314d1844
kmate2105@stud.uni-obuda.hu <kmate2105@stud.uni-obuda.hu>
2022-05-01 19:28:22 +0200

    Client crud methods added

--- ATYXSN_HFT_2021222.Client/Program.cs
+++ ATYXSN_HFT_2021222.Client/Program.cs
@@ -1,5 +1,7 @@
-﻿using ConsoleTools;
+﻿using ATYXSN_HFT_2021222.Models;
+using ConsoleTools;
 using System;
+using System.Collections.Generic;
 
 namespace ATYXSN_HFT_2021222.Client
 {
@@ -7,61 +9,37 @@ namespace ATYXSN_HFT_2021222.Client
     {
         static void Main(string[] args)
         {
-            static void Create(string entity)
-            {
-                Console.WriteLine(entity + " create");
-                Console.ReadLine();
-            }
-            static void List(string entity)
-            {
-                if (entity == "Actor")
-                {
+            RestService rest = new RestService("http://localhost:48810/", "match");
+            CrudService crud = new CrudService(rest);
 
-                }
-                Console.ReadLine();
-            }
-            static void Update(string entity)
-            {
-                Console.WriteLine(entity + " update");
-                Console.ReadLine();
-            }
-            static void Delete(string entity)
-            {
-                Console.WriteLine(entity + " delete");
-                Console.ReadLine();
-            }
+            var matchSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => crud.List<Match>())
+                .Add("Create", () => crud.Create<Match>())
+                .Add("Delete", () => crud.Delete<Match>())
+                .Add("Update", () => crud.Update<Match>())
+                .Add("Exit", ConsoleMenu.Close);
 
-            static void Main(string[] args)
-            {
-                var matchSubMenu = new ConsoleMenu(args, level: 1)
-                    .Add("List", () => List("Match"))
-                    .Add("Create", () => Create("Match"))
-                    .Add("Delete", () => Delete("Match"))
-                    .Add("Update", () => Update("Match"))
-                    .Add("Exit", ConsoleMenu.Close);
+            var bettorSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => crud.List<Bettor>())
+                .Add("Create", () => crud.Create<Bettor>())
+                .Add("Delete", () => crud.Delete<Bettor>())
+                .Add("Update", () => crud.Update<Bettor>())
+                .Add("Exit", ConsoleMenu.Close);
 
-                var bettorSubMenu = new ConsoleMenu(args, level: 1)
-                    .Add("List", () => List("Bettor"))
-                    .Add("Create", () => Create("Bettor"))
-                    .Add("Delete", () => Delete("Bettor"))
-                    .Add("Update", () => Update("Bettor"))
-                    .Add("Exit", ConsoleMenu.Close);
+            var bookmakerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => crud.List<Bookmaker>())
+                .Add("Create", () => crud.Create<Bookmaker>())
+                .Add("Delete", () => crud.Delete<Bookmaker>())
+                .Add("Update", () => crud.Update<Bookmaker>())
+                .Add("Exit", ConsoleMenu.Close);
 
-                var bookmakerSubMenu = new ConsoleMenu(args, level: 1)
-                    .Add("List", () => List("Bookmaker"))
-                    .Add("Create", () => Create("Bookmaker"))
-                    .Add("Delete", () => Delete("Bookmaker"))
-                    .Add("Update", () => Update("Bookmaker"))
-                    .Add("Exit", ConsoleMenu.Close);
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Matches", () => matchSubMenu.Show())
+                .Add("Bettors", () => bettorSubMenu.Show())
+                .Add("Bookmakers", () => bookmakerSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
 
-                var menu = new ConsoleMenu(args, level: 0)
-                    .Add("Matches", () => matchSubMenu.Show())
-                    .Add("Bettors", () => bettorSubMenu.Show())
-                    .Add("Bookmakers", () => bookmakerSubMenu.Show())
-                    .Add("Exit", ConsoleMenu.Close);
-
-                menu.Show();
-            }
+            menu.Show();
         }
     }
 }
