b94e4f238bb9150b483ac0c63c03c9daa0868abe
kmate2105@stud.uni-obuda.hu <kmate2105@stud.uni-obuda.hu>
2022-05-02 14:00:48 +0200

    Controller bug fixed

---
+++ ATYXSN_HFT_2021222.Endpoint/Controllers/BettorStatController.cs
@@ -0,0 +1,24 @@
+﻿using ATYXSN_HFT_2021222.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ATYXSN_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class BettorStatController : ControllerBase
+    {
+        IBettorLogic logic;
+
+        public BettorStatController(IBettorLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int>> BetsByBookmaker()
+        {
+            return this.logic.BetsByBookmaker();
+        }
+    }
+}
---
+++ ATYXSN_HFT_2021222.Endpoint/Controllers/MatchStatController.cs
@@ -0,0 +1,42 @@
+﻿using ATYXSN_HFT_2021222.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ATYXSN_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class MatchStatController : ControllerBase
+    {
+        IMatchLogic logic;
+
+        public MatchStatController(IMatchLogic matchLogic)
+        {
+            this.logic = matchLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> AverageOddsByBookmaker()
+        {
+            return this.logic.AverageOddsByBookmaker();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int>> MatchOffersByBookmaker()
+        {
+            return this.logic.MatchOffersByBookmaker();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> BiggestOddsByBookmaker()
+        {
+            return this.logic.BiggestOddsByBookmaker();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int>> NumberOfDrawsByBookmaker()
+        {
+            return this.logic.NumberOfDrawsByBookmaker();
+        }
+    }
+}
