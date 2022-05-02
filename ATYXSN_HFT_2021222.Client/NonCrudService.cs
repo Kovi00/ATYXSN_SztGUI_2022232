56df9a68b7577d03d2491d067b13ef2159e05751
kmate2105@stud.uni-obuda.hu <kmate2105@stud.uni-obuda.hu>
2022-05-02 21:38:38 +0200

    Method name changed

--- ATYXSN_HFT_2021222.Client/NonCrudService.cs
+++ ATYXSN_HFT_2021222.Client/NonCrudService.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ATYXSN_HFT_2021222.Models.Bettor;
+using static ATYXSN_HFT_2021222.Models.Match;
 
 namespace ATYXSN_HFT_2021222.Client
 {
@@ -18,55 +20,55 @@ namespace ATYXSN_HFT_2021222.Client
 
         public void AverageOddsByBookmaker()
         {
-            var items = rest.Get<KeyValuePair<string, double>>("MatchStat/AverageOddsByBookmaker");
+            var items = rest.Get<OddsInfo>("MatchStat/AverageOddsByBookmaker");
             Console.WriteLine("Bookmaker\tOdds");
             foreach (var item in items)
             {
-                Console.WriteLine($"{item.Key}\t{item.Value}");
+                Console.WriteLine($"{item.Name}\t{item.Odds}");
             }
             Console.ReadLine();
         }
 
         public void MatchOffersByBookmaker()
         {
-            var items = rest.Get<KeyValuePair<string, int>>("MatchStat/MatchOffersByBookmaker");
+            var items = rest.Get<MatchInfo>("MatchStat/MatchOffersByBookmaker");
             Console.WriteLine("Bookmaker\tMatches");
             foreach (var item in items)
             {
-                Console.WriteLine($"{item.Key}\t{item.Value}");
+                Console.WriteLine($"{item.Name}\t{item.Matches}");
             }
             Console.ReadLine();
         }
 
         public void BiggestOddsByBookmaker()
         {
-            var items = rest.Get<KeyValuePair<string, double>>("MatchStat/BiggestOddsByBookmaker");
+            var items = rest.Get<BiggestOdds>("MatchStat/BiggestOddsByBookmaker");
             Console.WriteLine("Bookmaker\tOdds");
             foreach (var item in items)
             {
-                Console.WriteLine($"{item.Key}\t{item.Value}");
+                Console.WriteLine($"{item.Name}\t{item.Odds}");
             }
             Console.ReadLine();
         }
 
         public void NumberOfDrawsByBookmaker()
         {
-            var items = rest.Get<KeyValuePair<string, int>>("MatchStat/NumberOfDrawsByBookmaker");
+            var items = rest.Get<DrawInfo>("MatchStat/NumberOfDrawsByBookmaker");
             Console.WriteLine("Bookmaker\tDraws");
             foreach (var item in items)
             {
-                Console.WriteLine($"{item.Key}\t{item.Value}");
+                Console.WriteLine($"{item.Name}\t{item.Draws}");
             }
             Console.ReadLine();
         }
 
         public void BetsByBookmaker()
         {
-            var items = rest.Get<KeyValuePair<string, int>>("BettorStat/BetsByBookmaker");
+            var items = rest.Get<BetInfo>("BettorStat/BetsByBookmaker");
             Console.WriteLine("Bookmaker\tOdds");
             foreach (var item in items)
             {
-                Console.WriteLine($"{item.Key}\t{item.Value}");
+                Console.WriteLine($"{item.Name}\t{item.Count}");
             }
             Console.ReadLine();
         }
